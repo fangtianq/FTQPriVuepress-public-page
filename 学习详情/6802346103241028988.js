db13b8ac20f477cb14a3e85b3c8bb72e149d261c
fangtianq <fangtianq@users.noreply.github.com>
2025-08-28 11:12:39 +0000

    Deploying to gh-pages from @ fangtianq/FTQPriVuepress-public@0aff4773234e8f09fb095c6db6ab63ea472e8c97 🚀

---
+++ 学习详情/6802346103241028988.js
@@ -0,0 +1,440 @@
+{
+  "url": "https://article.xuexi.cn/articles/index.html?art_id=6802346103241028988&item_id=6802346103241028988",
+  "image": [
+    {
+      "url": "https://boot-img.xuexi.cn/image/1004/process/0b5b2c8744cd4056bba6ef8983c90508.jpg",
+      "desc": "",
+      "type": "jpg",
+      "width": 600,
+      "height": 1898,
+      "yms_id": "14103951515530076161311004",
+      "image_id": "14103951515530076161311004",
+      "image_size": 230567,
+      "verify_hash": "8554340579241294708"
+    }
+  ],
+  "title": "近400亿人次！1至7月我国交通出行火热",
+  "audios": [],
+  "source": "yres-article-manual_新华社",
+  "videos": [
+    {
+      "thumbnails": [
+        {
+          "data": [
+            {
+              "url": "https://boot-img.xuexi.cn/image/1005/process/f6a52fe9fdfd473da6ebae9bc0175c9b.jpg",
+              "type": "jpg",
+              "width": 802,
+              "height": 451,
+              "image_id": "14105169082975027205281005",
+              "image_size": 118120,
+              "verify_hash": "11518372445241508983"
+            }
+          ],
+          "specification": "news-tab"
+        }
+      ],
+      "video_storage_info": [
+        {
+          "md5": "8620430542174414957",
+          "size": 2047557,
+          "type": 3,
+          "width": 648,
+          "format": "m3u8",
+          "height": 864,
+          "normal": "https://boot-video.xuexi.cn/video/1011/p/14103951521108582402441011/8d6ec90f996544e198433597eddfeeb6-2.m3u8",
+          "bitrate": 791,
+          "resolution": "2"
+        },
+        {
+          "md5": "13423230438764109258",
+          "size": 2201638,
+          "type": 2,
+          "width": 648,
+          "format": "mov,mp4,m4a,3gp,3g2,mj2",
+          "height": 864,
+          "normal": "https://boot-video.xuexi.cn/video/1011/p/849f7523d12584b67d01e8ea457c4ed1-8d6ec90f996544e198433597eddfeeb6-2.mp4",
+          "bitrate": 849,
+          "resolution": "2"
+        }
+      ],
+      "video_meta_settings": {
+        "ums_id": "14103951510413762567701011",
+        "summary": "",
+        "can_play": true,
+        "play_count": 0,
+        "play_length": 20
+      }
+    }
+  ],
+  "voices": [
+    {
+      "voice_storage_info": [
+        {
+          "md5": "87b5821a743dc772d9e92745a3626d81",
+          "url": "https://boot-video.xuexi.cn/voice/14105216933985402887461005/p/87b5821a743dc772d9e92745a3626d81-794ad65c44d548079125297d123d757e-2.mp3",
+          "size": 2537913,
+          "crc64": "7110340517244344782",
+          "format": "MP3",
+          "bitrate": 128,
+          "resolution": "2",
+          "play_length": 158
+        }
+      ],
+      "voice_meta_settings": {
+        "ums_id": "14105216933985402887461005",
+        "voice_language": "cantonese",
+        "voice_people_name": "xiaomei",
+        "voice_people_gender": "女"
+      }
+    },
+    {
+      "voice_storage_info": [
+        {
+          "md5": "9c7b8fa8129533bc11da1d67567ca514",
+          "url": "https://boot-video.xuexi.cn/voice/14105216933985935364141005/p/9c7b8fa8129533bc11da1d67567ca514-30f6c3c74c4d4042946e121838c03eaf-2.mp3",
+          "size": 2260806,
+          "crc64": "856748625858831803",
+          "format": "MP3",
+          "bitrate": 128,
+          "resolution": "2",
+          "play_length": 141
+        }
+      ],
+      "voice_meta_settings": {
+        "ums_id": "14105216933985935364141005",
+        "voice_language": "hakka",
+        "voice_people_name": "ZiJie_F_MKT",
+        "voice_people_gender": "女"
+      }
+    },
+    {
+      "voice_storage_info": [
+        {
+          "md5": "fcb3afb2c8ac5bcb7bbc3f29e54a9f45",
+          "url": "https://boot-video.xuexi.cn/voice/14105216933776424961371005/p/fcb3afb2c8ac5bcb7bbc3f29e54a9f45-0f7a0f91e7fa439aa5ee4c8d392d12ca-2.mp3",
+          "size": 2096130,
+          "crc64": "11243426168444699298",
+          "format": "mp3",
+          "bitrate": 128,
+          "resolution": "2",
+          "play_length": 130
+        }
+      ],
+      "voice_meta_settings": {
+        "ums_id": "14105216933776424961371005",
+        "voice_language": "mandarin",
+        "voice_people_name": "zhaolei",
+        "voice_people_gender": "女"
+      }
+    },
+    {
+      "voice_storage_info": [
+        {
+          "md5": "0d7cbfb8c2ad13d53d85b51e5c72cdcf",
+          "url": "https://boot-video.xuexi.cn/voice/14105217011328409602521005/p/0d7cbfb8c2ad13d53d85b51e5c72cdcf-916ab37471fc48d2990edb64303d8c8e-2.mp3",
+          "size": 3016476,
+          "crc64": "17388812611678722612",
+          "format": "mp3",
+          "bitrate": 128,
+          "resolution": "2",
+          "play_length": 188
+        }
+      ],
+      "voice_meta_settings": {
+        "ums_id": "14105217011328409602521005",
+        "voice_language": "english",
+        "voice_people_name": "pe_Catherine",
+        "voice_people_gender": "女"
+      }
+    },
+    {
+      "voice_storage_info": [
+        {
+          "md5": "0aad03ed7b9cffbede4e7060315af738",
+          "url": "https://boot-video.xuexi.cn/voice/14105217079822090241371005/p/0aad03ed7b9cffbede4e7060315af738-80540d26680d498d985abac9ae3552cd-2.mp3",
+          "size": 2843023,
+          "crc64": "10028580418779092730",
+          "format": "MP3",
+          "bitrate": 128,
+          "resolution": "2",
+          "play_length": 177
+        }
+      ],
+      "voice_meta_settings": {
+        "ums_id": "14105217079822090241371005",
+        "voice_language": "tibetan",
+        "voice_people_name": "sgron_dkar",
+        "voice_people_gender": "女"
+      }
+    },
+    {
+      "voice_storage_info": [
+        {
+          "md5": "501bf43f0bbd5af86efd7f21cda81c38",
+          "url": "https://boot-video.xuexi.cn/voice/14105217079653621762521005/p/501bf43f0bbd5af86efd7f21cda81c38-d9f4831d25444a628d17a0de083c38e6-2.mp3",
+          "size": 3222112,
+          "crc64": "7990246282499079911",
+          "format": "mp3",
+          "bitrate": 128,
+          "resolution": "2",
+          "play_length": 201
+        }
+      ],
+      "voice_meta_settings": {
+        "ums_id": "14105217079653621762521005",
+        "voice_language": "uyghur",
+        "voice_people_name": "patiguli",
+        "voice_people_gender": "女"
+      }
+    }
+  ],
+  "ai_info": {},
+  "content": "<!--{video:0}-->\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">新华社北京8月27日电 今年1至7月，我国跨区域人员流动量达394.6亿人次，同比增长3.9%。</p>\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">这是记者27日从交通运输部举行的例行新闻发布会上了解到的。</p>\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">“7月份，完成跨区域人员流动量57.1亿人次，同比增长2.2%，增速较6月回升0.7个百分点。分方式看，铁路、水路、民航客运量同比分别增长6.6%、2.1%和3.9%，公路人员流动量同比增长1.8%。”交通运输部新闻发言人李颖说。</p>\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">货运量保持平稳增长。李颖介绍，1至7月，我国完成营业性货运量330亿吨，同比增长3.8%。其中7月份完成营业性货运量49.7亿吨，同比增长3.4%，增速较6月回升0.5个百分点。</p>\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">港口货物吞吐量增长较快。1至7月，我国完成港口货物吞吐量104.4亿吨、同比增长4.4%，完成集装箱吞吐量2亿标箱、同比增长6.2%。其中7月份完成港口货物吞吐量15.4亿吨，同比增长6.9%，增速较6月加快2.2个百分点；完成港口集装箱吞吐量2996万标箱，同比增长2.7%。</p>\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">交通固定资产投资规模保持高位。李颖表示，1至7月，我国完成交通固定资产投资1.95万亿元。其中7月份完成交通固定资产投资3061亿元。分方式看，铁路完成投资771亿元，公路完成投资2005亿元，水路完成投资173亿元，民航完成投资112亿元。</p>\n\n<p class=\"text_align-justify text_indent-2em\" data-source=\"cke\">“总的来看，今年7月，交通运输经济运行延续回升向好的态势。”李颖说。（记者叶昊鸣）</p>\n\n<!--{img:0}-->\n",
+  "gonggao": {},
+  "options": {
+    "allow_show_ai_guide": false,
+    "allow_show_pc_ai_guide": false
+  },
+  "summary": "",
+  "category": [
+    "未分类"
+  ],
+  "identity": {
+    "type": "kNews",
+    "item_id": "6802346103241028988",
+    "platform": 0,
+    "producer": "yres-article-manual",
+    "creator_dept": "",
+    "creator_name": "",
+    "editors_name": [
+      "刘春雨"
+    ],
+    "root_item_id": "15607494945754310969",
+    "modifier_name": "刘春雨",
+    "publish_target": [
+      1,
+      2
+    ],
+    "create_timestamp": 1756334823626,
+    "modify_timestamp": 1756336277712,
+    "creator_contact_info": ""
+  },
+  "key_word": [],
+  "nlp_info": {
+    "53": [
+      "CgbnpL7kvJoSETI1NDkyMDk1Nzg0ODY1MzIy"
+    ],
+    "tag": {
+      "norm": 2.229932241080169,
+      "feature": [
+        {
+          "18": [
+            0
+          ],
+          "op_id": "15699958104736437",
+          "weight": 0.9823733568191528,
+          "literal": "label:交通出行",
+          "category": "IP",
+          "op_is_set": true,
+          "confidence": 0,
+          "op_type_id": 88
+        },
+        {
+          "18": [
+            0
+          ],
+          "op_id": "61230193240095386",
+          "weight": 0.8248645663261414,
+          "literal": "label:交通部",
+          "category": "ORG",
+          "op_is_set": true,
+          "confidence": 2,
+          "op_type_id": 85
+        },
+        {
+          "18": [
+            0
+          ],
+          "op_id": "7921667060347070",
+          "weight": 0.5621886253356934,
+          "literal": "label:交通运输",
+          "category": "NORMAL",
+          "op_is_set": true,
+          "confidence": 3,
+          "op_type_id": 30
+        },
+        {
+          "18": [
+            0
+          ],
+          "op_id": "27664471929702627",
+          "weight": 0.3792935013771057,
+          "literal": "label:交通运输经济",
+          "category": "NORMAL",
+          "op_is_set": true,
+          "confidence": 3,
+          "op_type_id": 94
+        },
+        {
+          "18": [
+            0
+          ],
+          "op_id": "22362854129635073",
+          "weight": 0.2293410897254944,
+          "literal": "label:人员流动",
+          "category": "NORMAL",
+          "op_is_set": true,
+          "confidence": 3,
+          "op_type_id": 66
+        },
+        {
+          "18": [
+            0
+          ],
+          "op_id": "22362854667305581",
+          "weight": 0.203193336725235,
+          "literal": "label:固定资产投资",
+          "category": "NORMAL",
+          "op_is_set": true,
+          "confidence": 3,
+          "op_type_id": 66
+        },
+        {
+          "18": [
+            0
+          ],
+          "op_id": "42866419194377084",
+          "weight": 0.1751250922679901,
+          "literal": "label:集装箱吞吐量",
+          "category": "NORMAL",
+          "op_is_set": true,
+          "confidence": 3,
+          "op_type_id": 66
+        }
+      ]
+    },
+    "category": [
+      "社会"
+    ]
+  },
+  "show_tag": [
+    "李颖",
+    "交通出行"
+  ],
+  "tag_info": {
+    "marking_suggest": [
+      {
+        "id": "0",
+        "name": "要闻",
+        "type": 0
+      }
+    ]
+  },
+  "sub_title": "",
+  "head_title": "",
+  "manual_tag": [],
+  "op_channel": [
+    800,
+    112
+  ],
+  "pc_channel": [
+    "1jscb6pu1n2"
+  ],
+  "share_info": {
+    "desc": "",
+    "image": {
+      "url": "https://boot-img.xuexi.cn/image/1005/process/a6016d9a42a74244a2ffe5a2121bb7cb.jpg",
+      "type": "jpg",
+      "width": 300,
+      "height": 300,
+      "image_id": "14087801429684019204141005",
+      "image_size": 29091,
+      "verify_hash": "2740704832787957530"
+    }
+  },
+  "style_type": 0,
+  "thumbnails": [],
+  "trace_info": {
+    "trace_id": "e4100882-8c80-48be-936e-a224c5598c58"
+  },
+  "is_external": false,
+  "marking_tag": [
+    {
+      "id": "7921667060347070",
+      "name": "交通运输",
+      "type": 1,
+      "is_valid": 1,
+      "entity_type_id": 30
+    },
+    {
+      "id": "27664471929702627",
+      "name": "交通运输经济",
+      "type": 1,
+      "is_valid": 1,
+      "entity_type_id": 94
+    },
+    {
+      "id": "15699958104736437",
+      "name": "交通出行",
+      "type": 1,
+      "is_valid": 1,
+      "entity_type_id": 88
+    },
+    {
+      "id": "22362854667305581",
+      "name": "固定资产投资",
+      "type": 1,
+      "is_valid": 1,
+      "entity_type_id": 66
+    },
+    {
+      "id": "22362854129635073",
+      "name": "人员流动",
+      "type": 1,
+      "is_valid": 1,
+      "entity_type_id": 66
+    }
+  ],
+  "orig_source": "新华网",
+  "pcsite_info": {
+    "sort": 0,
+    "title": "近400亿人次！1至7月我国交通出行火热",
+    "number": "",
+    "detail_title": "近400亿人次！1至7月我国交通出行火热"
+  },
+  "show_source": "新华社",
+  "link_structs": [],
+  "publish_time": "2025-08-28 07:10:52",
+  "article_links": [],
+  "audit_setting": {
+    "specified_audit": {
+      "send_person_id": "9189303037289908202",
+      "audit_person_id": "1a39f248b8a04d26e7c8c6256b9243e223f89f32215c383933fc7c44c846e254",
+      "send_person_name": "刘春雨"
+    }
+  },
+  "multi_category": {},
+  "event_locations": [],
+  "region_restrict": "",
+  "marking_category": [
+    {
+      "id": "25492238536667019",
+      "name": "经济"
+    }
+  ],
+  "normalized_title": "近400亿人次!1至7月我国交通出行火热",
+  "show_marking_tag": [],
+  "normalized_content": "新华社北京8月27日电 今年1至7月,我国跨区域人员流动量达394.6亿人次,同比增长3.9%。 这是记者27日从交通运输部举行的例行新闻发布会上了解到的。 “7月份,完成跨区域人员流动量57.1亿人次,同比增长2.2%,增速较6月回升0.7个百分点。分方式看,铁路、水路、民航客运量同比分别增长6.6%、2.1%和3.9%,公路人员流动量同比增长1.8%。”交通运输部新闻发言人李颖说。 货运量保持平稳增长。李颖介绍,1至7月,我国完成营业性货运量330亿吨,同比增长3.8%。其中7月份完成营业性货运量49.7亿吨,同比增长3.4%,增速较6月回升0.5个百分点。 港口货物吞吐量增长较快。1至7月,我国完成港口货物吞吐量104.4亿吨、同比增长4.4%,完成集装箱吞吐量2亿标箱、同比增长6.2%。其中7月份完成港口货物吞吐量15.4亿吨,同比增长6.9%,增速较6月加快2.2个百分点;完成港口集装箱吞吐量2996万标箱,同比增长2.7%。 交通固定资产投资规模保持高位。李颖表示,1至7月,我国完成交通固定资产投资1.95万亿元。其中7月份完成交通固定资产投资3061亿元。分方式看,铁路完成投资771亿元,公路完成投资2005亿元,水路完成投资173亿元,民航完成投资112亿元。 “总的来看,今年7月,交通运输经济运行延续回升向好的态势。”李颖说。(记者叶昊鸣)",
+  "machine_marking_category": [
+    {
+      "id": "25492095784865322",
+      "name": "社会"
+    }
+  ],
+  "item_info_extend": {
+    "send_task_id": "kNews-fa1b38e885dd4464b1c05816881f3359"
+  },
+  "pc_url": "https://www.xuexi.cn/lgpage/detail/index.html?id=6802346103241028988&item_id=6802346103241028988",
+  "data_js_url": "https://article.xuexi.cn/data/app/6802346103241028988.js"
+}
